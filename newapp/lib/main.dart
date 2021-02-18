a790481c4bb5e20c6c228e740e0e7f52db5eafba
Vrutik Parmar <parmarvrutik1@gmail.com>
2021-02-18 12:55:54 +0530

    commit on day-10

--- newapp/lib/main.dart
+++ newapp/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
    
     routes: {
-      "/":(context) => HomePage(),
+      "/":(context) => LoginPage(),
       MyRoutes.homeRoute:(context) => HomePage(),
       MyRoutes.loginRoute:(context) => LoginPage(),
       },
